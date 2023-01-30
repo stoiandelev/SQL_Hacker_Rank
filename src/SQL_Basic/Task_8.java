0538ea4c288d123e3c92d66ea733af35f13bb7bf
stoiandelev <stoian.delev@yahoo.com>
2023-01-30 22:14:42 +0200

    Use district and mod for divide

---
+++ src/SQL_Basic/Task_8.java
@@ -0,0 +1,16 @@
+package SQL_Basic;
+
+public class Task_8 {
+
+    //District for no repeats in query
+    //mod(id, 2) = 0 -> all even id number
+
+    /**
+     * Query a list of CITY names from STATION for cities that have an even ID number.
+     * Print the results in any order, but exclude duplicates from the answer.
+     * The STATION table is described as follows:
+     */
+
+    //SELECT DISTINCT CITY from STATION WHERE MOD(id,2) = 0;
+
+}
