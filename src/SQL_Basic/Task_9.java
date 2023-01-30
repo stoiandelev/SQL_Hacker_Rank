7fad8984daacef056a893b8825c6b5f7e9f3a08d
stoiandelev <stoian.delev@yahoo.com>
2023-01-30 22:21:10 +0200

    Use count and district

---
+++ src/SQL_Basic/Task_9.java
@@ -0,0 +1,15 @@
+package SQL_Basic;
+
+public class Task_9 {
+
+    //COUNT -> for all sum off column
+    //DISTINCT -> for no duplicate entries
+
+    /**
+     *Find the difference between the total number of CITY
+     * entries in the table and the number of distinct CITY entries in the table.
+     * The STATION table is described as follows:
+     */
+
+    //SELECT COUNT(CITY) - COUNT(DISTINCT(CITY)) FROM STATION;
+}
