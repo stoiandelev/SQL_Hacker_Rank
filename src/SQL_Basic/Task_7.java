4bb989a6531af947a67dd80a1c9576397502926b
stoiandelev <stoian.delev@yahoo.com>
2023-01-30 22:10:33 +0200

    Select multiple choice

---
+++ src/SQL_Basic/Task_7.java
@@ -0,0 +1,12 @@
+package SQL_Basic;
+
+public class Task_7 {
+
+    /**
+     *
+     * Query a list of CITY and STATE from the STATION table.
+     * The STATION table is described as follows:
+     */
+
+    //SELECT CITY,STATE FROM STATION;
+}
